9ad080adfb63c94a5349ed1e53524218e6ee17d7
Himanshu <himanshu.verma@zakoopi.com>
2015-06-09 13:56:26 +0530

    Released  Popular Feed API for Android

--- src/Model/Entity/FeedPopular.php
+++ src/Model/Entity/FeedPopular.php
@@ -21,22 +21,33 @@ class FeedPopular extends Entity
         'timestamp' => true
     ];
     protected $_virtual = [
-        'data'
+        'articleData',
+        'lookbookData',
+        'storeReviewData',
+        'teamData'
     ];
-    
-    protected function _getData(){
+
+
+    protected function _getModel(){
         $table = \Cake\ORM\TableRegistry::get($this->_properties['model']);
         if($this->_properties['model'] == 'Articles'){
             $result = $table->find()->contain(['Cities','Users','ArticleImages'])->where(['Articles.id'=>$this->_properties['key']])->first();
+            $this->_virtual[] = 'articleData';
+            $this->set('articleData', $result);
         }elseif($this->_properties['model'] == 'Lookbooks'){
             $result = $table->find()->contain(['Cards','Users'])->where(['Lookbooks.id'=>$this->_properties['key']])->first();
+            $this->_virtual[] = 'lookbookData';
+            $this->set('lookbookData', $result);
         }elseif($this->_properties['model'] == 'StoreReviews'){
             $result = $table->find()->contain(['Users','Stores','Stores.StoreImages'])->where(['StoreReviews.id'=>$this->_properties['key']])->first();
+            $this->_virtual[] = 'storeReviewData';
+            $this->set('storeReviewData', $result);
         }else{
             $result = $table->find()->where([$this->_properties['model'].'.id'=>$this->_properties['key']])->first();
+            $this->_virtual[] = 'teamData';
+            $this->set('teamData', $result);
         }
-        
-        return $result;
+        return $this->_properties['model'];// $result;
     }
     
 }
